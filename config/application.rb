2d2c7fcf64e398d7070e2408b4c165aed164024e
uchihiro <tohchi4122@gmail.com>
2022-05-23 15:47:26 +0900

    利用可能なロケール辞書の記述する順番を変更
    利用可能なロケール辞書の記述する順番について、日本語を先頭にしました。

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
-    I18n.available_locales = [:en, :ja]
+    I18n.available_locales = [:ja, :en]
     config.i18n.default_locale = :ja
     config.load_defaults 6.1
   end
