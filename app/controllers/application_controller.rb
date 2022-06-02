de13561bb40fe3e35a910424964aeab428133ae7
uchihiro <tohchi4122@gmail.com>
2022-06-02 21:06:50 +0900

    ストロングパラメーターを設定
    アカウント登録時およびアカウント情報更新時のストロングパラメーターを設定しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters , if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :post_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :post_code, :address, :self_introduction])
+  end
 end
