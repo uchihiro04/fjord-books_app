25299141fa17ac6da0f171e903d41429c265d56e
uchihiro <tohchi4122@gmail.com>
2022-06-03 14:00:18 +0900

    アカウント情報更新後、アカウント詳細画面にリダイレクトするように変更
    アカウント情報更新後のリダイレクト先を、本の一覧画面からアカウント詳細画面に変更しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :books
   resources :users, only: [:index, :show, :edit]
   root to: "books#index"
