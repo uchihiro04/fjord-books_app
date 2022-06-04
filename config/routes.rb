636a1c1d500381a75d3cf10be7e820ecb1755012
uchihiro <tohchi4122@gmail.com>
2022-06-04 14:14:06 +0900

    letter_opener_webに関する設定に関するコードを追加
    gemのletter_opener_webを利用するために設定に関するコードを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :books
   resources :users, only: [:index, :show, :edit]
   root to: "books#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
