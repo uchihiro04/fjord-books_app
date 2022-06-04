f99527b0856cf80149d629e458f7738267cda042
uchihiro <tohchi4122@gmail.com>
2022-06-04 14:17:12 +0900

    未ログインの状態でページを閲覧できない状態に修正
    未ログインの状態で本やユーザーに関するページが閲覧できないように修正しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters , if: :devise_controller?
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :post_code, :address, :self_introduction])
     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :post_code, :address, :self_introduction])
   end
+
 end
