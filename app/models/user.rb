6ad699a91f997faceb5ea6f663622c759d1cdbd0
uchihiro <tohchi4122@gmail.com>
2022-06-15 20:30:39 +0900

    UserモデルとFollowモデルの関連付けを実装
    UserモデルとFollowモデルの関連付けを行いました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :active_follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy 
+  has_many :passive_follows, class_name: 'Follow', foreign_key: 'following_id', dependent: :destroy
+  has_many :followings, through: :active_follows
+  has_many :followers, through: :passive_follows
+
 end
