e0f2fa2f8dae1fc399c9bedae6c41aa8993c477e
uchihiro <tohchi4122@gmail.com>
2022-06-24 21:18:06 +0900

    UserモデルとReportのモデルの関連付けを追加
    UserモデルとReportモデルを一対多で関連付けを追加しました。

--- db/migrate/20220622132113_create_reports.rb
+++ db/migrate/20220622132113_create_reports.rb
@@ -3,6 +3,7 @@ class CreateReports < ActiveRecord::Migration[6.1]
     create_table :reports do |t|
       t.string :title
       t.text :content
+      t.references :user, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,10 @@ ActiveRecord::Schema.define(version: 2022_06_22_132113) do
   create_table "reports", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -83,4 +85,5 @@ ActiveRecord::Schema.define(version: 2022_06_22_132113) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "reports", "users"
 end
