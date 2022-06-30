af3adf17e91e1d76cc6cbe9cf04bf406f807f854
uchihiro <tohchi4122@gmail.com>
2022-06-30 11:39:44 +0900

    コメント一覧に表示する項目を変更
    コメント一覧に名前（名前が未登録の場合はメールアドレス）、投稿日、コメント内容が表示されるように変更しました。

--- db/migrate/20220625113753_create_comments.rb
+++ db/migrate/20220625113753_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.text :content
-      t.references :user, foreign_key: true, null: false
+      t.references :user, foreign_key: true
       t.references :commentable, polymorphic: true, null: false
 
       t.timestamps
