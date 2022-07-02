baf1dc88e7ede37cdfe41c1800f7aa6fd900717d
uchihiro <tohchi4122@gmail.com>
2022-07-02 22:40:36 +0900

    バリデーションを追加
    NOT NULL制約がついているカラムに関して、空でないことを確認するバリデーションを追加しました。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  validates :content, presence: true
+
+  with_options presence: true do
+    validates :content
+    validates :commentable_type
+    validates :commentable_id
+  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,5 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  validates :user_id, presence: true
 end
