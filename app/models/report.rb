6cae870b81f4eb1eea730b5acf74bb993511e40c
uchihiro <tohchi4122@gmail.com>
2022-07-12 22:01:18 +0900

    日報のタイトル・本文が未入力の場合投稿ができないように変更
    日報のタイトル・本文が未入力の場合、「このフィールドを入力してください。」とメッセージが表示されるように変更しました。また、commentsテーブルのtitle、contentカラムにNOT NULL制約およびバリデーションを追加しました。

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,6 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -70,8 +70,8 @@ ActiveRecord::Schema.define(version: 2022_06_25_113753) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
