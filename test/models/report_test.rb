41c66e45bec48437b0551e8e294ecc9ba4d91297
uchihiro <tohchi4122@gmail.com>
2022-07-18 21:26:53 +0900

    Reportモデル内のメソッドに関するモデルテストを作成
    Reportモデル内にあるメソッドについて、モデルテストを作成しました。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,21 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @alice = users(:alice)
+  end
+
+  test 'determine if editing is allowed' do
+    bob = users(:bob)
+    report = reports(:report_one)
+    assert report.editable?(@alice)
+    assert_not report.editable?(bob)
+  end
+
+  test 'generate_date' do
+    travel_to Time.zone.local(2022, 7, 1, 12, 0, 0) do
+      report = Report.create(user_id: @alice.id, title: '初日報', content: '初日報です')
+      assert_equal 'Fri, 01 Jul 2022'.to_date, report.created_on
+    end
+  end
 end
