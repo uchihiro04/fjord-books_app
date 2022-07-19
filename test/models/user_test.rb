c9e40e75c535a6710133228c5da738d45e8b39bf
uchihiro <tohchi4122@gmail.com>
2022-07-19 14:53:51 +0900

    name_or_emailメソッドに関するテスト名を変更
    name_or_emailメソッドに関するテスト名について、名前かメールアドレスか判断するという意味の「determine if it is a name or email address」に変更しました。

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -33,7 +33,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @alice.following?(@bob)
   end
 
-  test 'name_or_email' do
+  test 'determine if it is a name or email address' do
     user = User.new(email: 'foo@example.com', name: '')
     assert_equal 'foo@example.com', user.name_or_email
 
